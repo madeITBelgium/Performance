6c73a5cf300b3b854cc1a148e5ff401066c6e4ad
Made I.T <info@madeit.be>
2022-10-20 13:06:17 +0200

    Update wp-db.php

--- wp-db.php
+++ wp-db.php
@@ -56,3 +56,46 @@ function test_db()
     }
 }
 add_action('admin_footer', 'test_db');
+
+function test_wp_db()
+{
+    global $wpdb, $_GET;
+
+    if (true || isset($_GET['debug'])) {
+        $nQrs = 0;
+        $totalTime = 0;
+        $maxTime = 0;
+        $maxQry = 0;
+        $maxIndex = null;
+        
+        $startTime = START_TIME;
+        $endTime = microtime(true);
+
+        foreach ($wpdb->queries as $index => $query) {
+            ++$nQrs;
+            $totalTime += $query[1];
+            if ($query[1] > $maxTime) {
+                $maxQry = $query;
+                $maxTime = $query[1];
+                $maxIndex = $index;
+            }
+        }
+        $fp = fopen('query.log', 'a');
+        fwrite($fp, 'Queries: '.$nQrs.' - Total Time:'.round($totalTime, 3).'s - maxTime: '.round($maxTime, 3).'s - Totale page time: '.round($endTime - $startTime, 3).'s' . "\n");
+        fwrite($fp, print_r($wpdb->queries[$maxIndex], true) . "\n");
+        fclose($fp);
+        
+        
+        echo '<div style="margin-left: 200px">';
+        echo 'Queries: '.$nQrs.' - Total Time:'.round($totalTime, 3).'s - maxTime: '.round($maxTime, 3).'s - Totale page time: '.round($endTime - $startTime, 3).'s <br>';
+        echo '<pre>';
+        print_r($maxQry);
+        echo '</pre>';
+
+        echo '<pre>';
+        print_r($wpdb->queries);
+        echo '</pre>';
+        echo '</div>';
+    }
+}
+add_action('wp_footer', 'test_wp_db');
